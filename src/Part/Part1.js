e0c7133feddf14d03a62e643d70b4c5f26a7441e
HyunJoo Kim <hyunjoogo@gmail.com>
2021-10-03 02:30:55 +0900

    Let's Make a Face Part II (React)

--- src/Part/Part1.js
+++ src/Part/Part1.js
@@ -1,23 +1,31 @@
 import React from "react";
 
- const Part1 = () => {
+const centerX = 480;
+const centerY = 250;
+const strokeWidth = 20;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 40;
+
+
+const Part1 = () => {
   return (
     <>
       <svg width="960" height="500">
-        <circle cx="480"
-                cy={"250"}
-                r="245"
+        <circle cx={centerX}
+                cy={centerY}
+                r={centerY - strokeWidth / 2}
                 fill={"yellow"}
                 stroke={"black"}
-                strokeWidth={"10"}
+                strokeWidth={strokeWidth}
         />
-        <circle cx="350"
-                cy="180"
-                r="50"
+        <circle cx={centerX - eyeOffsetX}
+                cy={centerY - eyeOffsetY}
+                r={eyeRadius}
         />
-        <circle cx="600"
-                cy="180"
-                r="50"
+        <circle cx={centerX + eyeOffsetX}
+                cy={centerY - eyeOffsetY}
+                r={eyeRadius}
         />
       </svg>
     </>
