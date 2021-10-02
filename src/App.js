cbe0e3de0bc9089d95da6de92d5a04777b26ae8c
HyunJoo Kim <hyunjoogo@gmail.com>
2021-10-03 02:11:39 +0900

    Let's Make a Face Part I (HTML, CSS, SVG)
    Let's Make a Face Part I (HTML, CSS, SVG)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Part1 from "./Part/Part1";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Part1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Part/Part1.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+ const Part1 = () => {
+  return (
+    <>
+      <svg width="960" height="500">
+        <circle cx="480"
+                cy={"250"}
+                r="245"
+                fill={"yellow"}
+                stroke={"black"}
+                strokeWidth={"10"}
+        />
+        <circle cx="350"
+                cy="180"
+                r="50"
+        />
+        <circle cx="600"
+                cy="180"
+                r="50"
+        />
+      </svg>
+    </>
+  )
+}
+export default Part1;
