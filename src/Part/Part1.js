582644a3dca5e0e6c351acedc8703ecd11c7c8de
HyunJoo Kim <hyunjoogo@gmail.com>
2021-10-04 13:48:12 +0900

    1:14:57 Let's Make a Face Part III (React & D3)

--- src/Part/Part1.js
+++ src/Part/Part1.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {arc} from 'd3';
+
 
 const centerX = 480;
 const centerY = 250;
@@ -6,27 +8,36 @@ const strokeWidth = 20;
 const eyeOffsetX = 90;
 const eyeOffsetY = 100;
 const eyeRadius = 40;
+const mouthWidth = 20;
+const mouthRadius = 140;
 
+const mouthArc = arc()
+  .innerRadius(mouthRadius) // 안쪽 원 반지름
+  .outerRadius(mouthWidth + mouthRadius) // 바깥쪽 원 반지름
+  .startAngle(Math.PI / 2) // 시작점 각도
+  .endAngle(Math.PI * 3 / 2);
 
 const Part1 = () => {
   return (
     <>
       <svg width="960" height="500">
-        <circle cx={centerX}
-                cy={centerY}
-                r={centerY - strokeWidth / 2}
-                fill={"yellow"}
-                stroke={"black"}
-                strokeWidth={strokeWidth}
-        />
-        <circle cx={centerX - eyeOffsetX}
-                cy={centerY - eyeOffsetY}
-                r={eyeRadius}
-        />
-        <circle cx={centerX + eyeOffsetX}
-                cy={centerY - eyeOffsetY}
-                r={eyeRadius}
-        />
+        <g transform={`translate(${centerX},${centerY})`}>
+          <circle
+            r={centerY - strokeWidth / 2}
+            fill={"yellow"}
+            stroke={"black"}
+            strokeWidth={strokeWidth}
+          />
+          <circle cx={-eyeOffsetX}
+                  cy={-eyeOffsetY}
+                  r={eyeRadius}
+          />
+          <circle cx={eyeOffsetX}
+                  cy={-eyeOffsetY}
+                  r={eyeRadius}
+          />
+          <path d={mouthArc()}/>
+        </g>
       </svg>
     </>
   )
