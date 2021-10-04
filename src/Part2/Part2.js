52484e32f49e12cba2fd185a8716c8f3829ac909
HyunJoo Kim <hyunjoogo@gmail.com>
2021-10-04 17:52:50 +0900

    2:48:28 Parsing CSV Data with D3

--- src/Part2/Part2.js
+++ src/Part2/Part2.js
@@ -1,15 +1,38 @@
 import React, {useCallback, useState} from 'react';
+import {csv, csvFormat} from "d3";
 
 
 const loadData = () => {
-  const fetchText = async (url) => {
-    const response = await fetch(url);
-    return await response.text();
-  }
-  const SCVURL = "https://gist.githubusercontent.com/hyunjoogo/60002b0cfa55940ba486ba8c55850b7d/raw/66b02acd4f34d6c6718dd535829b0e41e827dd30/cssNamedColors.csv"
+  const CSVURL = "https://gist.githubusercontent.com/hyunjoogo/60002b0cfa55940ba486ba8c55850b7d/raw/66b02acd4f34d6c6718dd535829b0e41e827dd30/cssNamedColors.csv"
+
+  // d3 기능으로 할 때
+  csv(CSVURL).then(data => {
+    let message = '';
+    message = message + Math.round(csvFormat(data).length / 1024) + ' kb\n';
+    message = message + data.length + ' rows\n';
+    message = message + data.columns.length + ' columns\n';
+    document.body.textContent = message;
+  })
+
+  // fetch, async, await 사용하였을 때
+  //   const fetchText = async (url) => {
+  //     const response = await fetch(url);
+  //     return await response.text();
+  //   }
+  //
+  //   fetchText(CSVURL)
+  //     .then(text => {
+  //       const data = csvParse(text);
+  //       let message = '';
+  //       message = message + Math.round(text.length / 1024) + ' kb\n';
+  //       message = message + data.length + ' rows\n';
+  //       message = message + data.columns.length + ' columns\n';
+  //       document.body.textContent = message;
+  //     });
 
 
-  fetchText(SCVURL).then(text => console.log(text));
+  // 피라미드 오브 둠 ㅋㅋㅋㅋㅋ
+  // indent 모양이 피라미드라서 ㅋㅋㅋ
   // fetch(url)
   //   .then(result => {
   //     result.text()
