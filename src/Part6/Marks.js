2f9a118b04c92d0b51239dfc26d12da661f3c83d
HyunJoo Kim <hyunjoogo@gmail.com>
2021-10-19 09:32:35 +0900

    part6

---
+++ src/Part6/Marks.js
@@ -0,0 +1,21 @@
+export const Marks = ({data, xScale, yScale, xValue, yValue, toolTipFormat, circleRadius}) =>
+  (
+    <>
+      <path d={line()
+        .x(d=> xScale(xValue(d)))}/>
+      
+
+
+      data.map((d, index) => (
+      <circle // 원 그리기
+        className="mark"
+        key={index}
+        cx={xScale(xValue(d))} // 원의 중심의 x좌표
+        cy={yScale(yValue(d))} // 원의 중심의 y좌표
+        r={circleRadius} // 반지름
+      >
+        <title>{toolTipFormat(d.Population)}</title>
+      </circle>
+      ))
+    </>
+  )
